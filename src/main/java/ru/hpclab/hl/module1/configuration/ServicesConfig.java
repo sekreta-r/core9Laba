7e307d15a6bdca98735e78168fd02ad5fbb46b1c
sekreta-r <liza.sek.000@gmail.com>
2025-04-24 10:03:23 +0300

    Lab 9

--- src/main/java/ru/hpclab/hl/module1/configuration/ServicesConfig.java
+++ src/main/java/ru/hpclab/hl/module1/configuration/ServicesConfig.java
@@ -1,14 +1,7 @@
 package ru.hpclab.hl.module1.configuration;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import ru.hpclab.hl.module1.model.User;
-//import ru.hpclab.hl.module1.repository.UserRepository;
-//import ru.hpclab.hl.module1.service.StatisticsService;
-//import ru.hpclab.hl.module1.service.UserService;
 
-import java.util.UUID;
 
 @Configuration
 public class ServicesConfig {
